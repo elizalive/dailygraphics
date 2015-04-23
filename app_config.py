93f974eb0e48c18b14c071672c911e261e44b4de
Christopher Groskopf <staringmonkey@gmail.com>
2015-04-23 13:29:05 -0400

    Add analytics. Closes #77.

--- app_config.py
+++ app_config.py
@@ -67,6 +67,10 @@ ASSETS_S3_BUCKET = {
 DEFAULT_MAX_AGE = 20
 ASSETS_MAX_AGE = 300
 
+GOOGLE_ANALYTICS = {
+    'ACCOUNT_ID': 'UA-5828686-75'
+}
+
 # These variables will be set at runtime. See configure_targets() below
 S3_BUCKET = None
 S3_BASE_URL = ''
